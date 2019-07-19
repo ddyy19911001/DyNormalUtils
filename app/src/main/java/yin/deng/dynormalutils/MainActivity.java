dde2e38f061b929f6c394ee284fff7cd7d53c527
liuye <liuye@51web.com>
2019-07-19 15:27:12 +0800

    首次提交

---
+++ app/src/main/java/yin/deng/dynormalutils/MainActivity.java
@@ -0,0 +1,26 @@
+package yin.deng.dynormalutils;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+import yin.deng.normalutils.utils.LogUtils;
+import yin.deng.normalutils.utils.SnackbarUtils;
+import yin.deng.normalutils.view.CircleImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+        SnackbarUtils.ShortSnackbar(tv,"测试一下消息",SnackbarUtils.Warning).show();
+    }
+}
